d27145c3aec2576b1ad60843814d3d3360656a25
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-10-20 19:51:30 +0200

    Improve some time optimization in read file

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: migugar2 <migugar2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:18:35 by migugar2          #+#    #+#             */
-/*   Updated: 2024/10/05 16:24:22 by migugar2         ###   ########.fr       */
+/*   Updated: 2024/10/20 19:48:29 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 char	*get_new_saved(char **saved, char *new_line, size_t line_len)
 {
@@ -62,9 +63,11 @@ char	*read_until_eol_eof(int fd, char **saved, char **buffer)
 {
 	char		*temp;
 	ssize_t		bytes_read;
+	char		*compared;
 
 	bytes_read = 1;
-	while (bytes_read > 0 && str_chr(*saved, '\n') == NULL)
+	compared = *saved;
+	while (bytes_read > 0 && str_chr(compared, '\n') == NULL)
 	{
 		bytes_read = read(fd, *buffer, BUFFER_SIZE);
 		if (bytes_read < 0)
@@ -72,6 +75,7 @@ char	*read_until_eol_eof(int fd, char **saved, char **buffer)
 		if (bytes_read > 0)
 		{
 			(*buffer)[bytes_read] = '\0';
+			compared = *buffer;
 			temp = str_join(*saved, *buffer);
 			if (temp == NULL)
 				return (free_str(saved), free_str(buffer));
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: migugar2 <migugar2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:41:29 by migugar2          #+#    #+#             */
-/*   Updated: 2024/10/05 18:49:34 by migugar2         ###   ########.fr       */
+/*   Updated: 2024/10/20 19:48:22 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,9 +62,11 @@ char	*read_until_eol_eof(int fd, char **saved, char **buffer)
 {
 	char		*temp;
 	ssize_t		bytes_read;
+	char		*compared;
 
 	bytes_read = 1;
-	while (bytes_read > 0 && str_chr(*saved, '\n') == NULL)
+	compared = *saved;
+	while (bytes_read > 0 && str_chr(compared, '\n') == NULL)
 	{
 		bytes_read = read(fd, *buffer, BUFFER_SIZE);
 		if (bytes_read < 0)
@@ -72,6 +74,7 @@ char	*read_until_eol_eof(int fd, char **saved, char **buffer)
 		if (bytes_read > 0)
 		{
 			(*buffer)[bytes_read] = '\0';
+			compared = *buffer;
 			temp = str_join(*saved, *buffer);
 			if (temp == NULL)
 				return (free_str(saved), free_str(buffer));
